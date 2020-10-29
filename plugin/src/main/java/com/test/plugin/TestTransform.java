f9d6d82ad08ce1430c1fa880a239053716c6ff1d
wanglun.skka <wanglun.skka@bytedance.com>
2020-10-29 19:57:16 +0800

    first commit

---
+++ plugin/src/main/java/com/test/plugin/TestTransform.java
@@ -0,0 +1,45 @@
+package com.test.plugin;
+
+import com.android.build.api.transform.QualifiedContent;
+import com.android.build.api.transform.Transform;
+import com.android.build.api.transform.TransformException;
+import com.android.build.api.transform.TransformInvocation;
+import com.android.build.gradle.internal.pipeline.TransformManager;
+
+import java.io.IOException;
+import java.util.Set;
+
+
+/**
+ * @author: wanglun
+ * @date: 2020/10/29
+ * @desc:
+ */
+public class TestTransform extends Transform {
+
+
+    @Override
+    public String getName() {
+        return "TestTransform";
+    }
+
+    @Override
+    public Set<QualifiedContent.ContentType> getInputTypes() {
+        return TransformManager.CONTENT_CLASS;
+    }
+
+    @Override
+    public Set<? super QualifiedContent.Scope> getScopes() {
+        return TransformManager.SCOPE_FULL_PROJECT;
+    }
+
+    @Override
+    public boolean isIncremental() {
+        return false;
+    }
+
+    @Override
+    public void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
+        super.transform(transformInvocation);
+    }
+}
